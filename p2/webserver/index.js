3cd5b94f43dd2ecdbd8392c3eb563de8a24dd6f4
gil <fxbullich@gmail.com>
2019-11-06 21:39:04 +0100

    [FEAT] es poden afegir sales cutres

--- p2/webserver/index.js
+++ p2/webserver/index.js
@@ -11,7 +11,21 @@ io.on('connection', function(socket){
   socket.on('chat message', function(msg){
   	socket.broadcast.emit('chat message', msg);
   });
+
+	socket.on('connect-room', function(id,name){
+		socket.join(id)
+    io.to(id).emit('join-message', name + ' has joined to room '+ id);
+  });
+
+	socket.on('room-message', function(id,msg){
+		socket.broadcast.to(id).emit('room-message', msg);	
+	});
+
 });
+
+
+
+
 
 
 http.listen(3000, function(){
